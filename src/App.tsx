642cf882e6135a7021d8273a4587bbd4b630b796
nick <nikoschatzoudas@gmail.com>
2025-10-10 02:54:21 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { AnimatedBackground } from "./components/animated-background";
-import ReactDOM from "react-dom";
+
 import { Canvas } from '@react-three/fiber';
 import Model from "./components/Model";
 
