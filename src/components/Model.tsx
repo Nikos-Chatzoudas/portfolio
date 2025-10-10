d21a1d4e02c72c5e7d8ec331ab631e27c2fe7a63
nick <nikoschatzoudas@gmail.com>
2025-10-10 03:58:40 +0300

    testing

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,28 +1,8 @@
 import { Html, Environment, OrbitControls, useGLTF } from '@react-three/drei';
-import { useControls } from 'leva';
 
 export default function Model() {
   const model = useGLTF('mainmodel.glb');
   const screen = model.scene.getObjectByName('Screen');
-
-  const { width, height, scale } = useControls('iframe', {
-    width: {
-      value: 1024,
-
-
-      step: 1
-    },
-    height: {
-      value: 600,
-
-      step: 1
-    },
-    scale: {
-      value: 1,
-
-      step: 0.1
-    }
-  });
 
   return (
     <>
@@ -41,12 +21,11 @@ export default function Model() {
           ]}
         >
           <iframe
-            src="https://os.chatzoudas.dev/"
-            style={{
-              width: `${width}px`,
-              height: `${height}px`,
-              transform: `scale(${scale})`,
-            }}
+            //https://www.youtube.com/embed/dQw4w9WgXcQ?si=dmq9EAcliiJYKKwp
+            //https://os.chatzoudas.dev 
+            src="https://os.chatzoudas.dev"
+
+
           />
         </Html>
       )}
