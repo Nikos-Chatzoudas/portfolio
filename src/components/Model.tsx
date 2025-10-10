8099bb7653110e4b690fb92be0d1a9e49b34f14d
nick <nikoschatzoudas@gmail.com>
2025-10-10 04:13:51 +0300

    testing 5

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,30 +1,10 @@
 import { Html, Environment, OrbitControls, useGLTF } from '@react-three/drei';
-import { useControls } from 'leva';
+
 
 export default function Model() {
   const model = useGLTF('mainmodel.glb');
   const screen = model.scene.getObjectByName('Screen');
 
-  const { width, height, scale } = useControls('iframe', {
-    width: {
-      value: 1024,
-      min: 100,
-      max: 2000,
-      step: 1
-    },
-    height: {
-      value: 600,
-      min: 100,
-      max: 1200,
-      step: 1
-    },
-    scale: {
-      value: 1,
-      min: 0.1,
-      max: 2,
-      step: 0.1
-    }
-  });
 
   return (
     <>
@@ -35,7 +15,7 @@ export default function Model() {
           transform
           occlude
           wrapperClass="model-fullscreen"
-          distanceFactor={scale}
+          distanceFactor={0.5}
           position={[
             screen.position.x,
             screen.position.y,
@@ -44,10 +24,7 @@ export default function Model() {
         >
           <iframe
             src="https://os.chatzoudas.dev"
-            style={{
-              width: `${width}px`,
-              height: `${height}px`,
-            }}
+
           />
         </Html>
       )}
